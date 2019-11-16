9a5e7e0919bb2b593c1a890c3d2c980bda12f5c5
Syhko <alexandregrauby@gmail.com>
2019-11-17 00:18:55 +0200

    modal

--- src/components/BuyModal.jsx
+++ src/components/BuyModal.jsx
@@ -2,12 +2,28 @@ import React from "react";
 import "./BuyModal.scss";
 
 const BuyModal = () => {
+  const [validate, setValidate] = React.useState(false);
   return (
     <>
       <div className="fading" />
       <div className="modal-wrapper">
-        <div className="left-choice">OK</div>
-        <div className="right-choice">OK</div>
+        {!validate && (
+          <>
+            <div className="left-choice" onClick={() => setValidate(true)}>
+              <p>Delivery ?</p>
+              <img src="https://s3.amazonaws.com/iconbros/icons/icon_pngs/000/000/163/original/delivery.png?1510082899" />
+            </div>
+            <div className="right-choice" onClick={() => setValidate(true)}>
+              <p>Pick up ?</p>
+              <img src="https://icon-library.net/images/pick-up-icon/pick-up-icon-23.jpg" />
+            </div>
+          </>
+        )}
+        {validate && (
+          <div className="validation">
+            <p>Thanks</p>
+          </div>
+        )}
       </div>
     </>
   );
