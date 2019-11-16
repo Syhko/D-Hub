98b6ca446a603592973be2bd3a4c3a6e1efa2d12
Erland <nicolasjonason@gmail.com>
2019-11-17 01:39:45 +0200

    scroll is clip

--- src/Mobile/MobilePage.jsx
+++ src/Mobile/MobilePage.jsx
@@ -5,7 +5,7 @@ import ProductFilter from './ProductFilter';
 
 const MobileView = ({ categories, selectCategory, category, products }) => {
 	return (
-		<div style={{ width: window.innerWidth, height: window.innerHeight, overflow: 'hidden' }}>
+		<div style={{ width: window.innerWidth, height: window.innerHeight, overflow: 'clip' }}>
 			<div
 				style={{
 					width: '100%',
